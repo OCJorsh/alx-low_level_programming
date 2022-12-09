3c2aa79fac32a7fc52cd8c0695464fc6e6f0822c
OCJorsh <maximizejosh@gmail.com>
2022-12-09 13:11:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
  *main - Entry point 
- *
- *Return: Always 0 (Success) 
+ *Return: Always 0 
  */
-
 int main(void)
 {
-	puts("\"Progamming is like a building a multilingual puzzle") :
-	return (O) :
+	puts("\"Progamming is like a building a multilingual puzzle");
+	return (O);
 }
