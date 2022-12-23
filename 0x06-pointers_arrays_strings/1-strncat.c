213d1f4437be52a2614edb82887b2ba2d5942f5d
OCJorsh <maximizejosh@gmail.com>
2022-12-22 17:30:24 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "kasiye.h"
+
+/**
+ * *_strcat - concentrate n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer of bytes
+ * @n: number of bytes to recieve
+ * Return: void
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int dest_len, i;
+
+	for (dest_len = 0; dest[dest_len] != '\0';)
+		dest_len++;
+
+	for (i = 0; i < n && src[i] != '\0';)
+		i++;
+	dest[dest_len + i] = src[i];
+
+	/*should end with a end of string char*/
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
