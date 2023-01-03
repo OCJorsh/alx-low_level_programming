4c0aeed73f42edb9234962d3e3255796fa51677a
OCJorsh <maximizejosh@gmail.com>
2023-01-03 07:01:27 -0800

    0-memset.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,26 @@
+#include <stdio.h>                                                                                                              
+
+                                                                                                                                
+
+/**                                                                                                                             
+ *
+ *  *main - Entry point                                                                                                            
+ *
+ *   *                                                                                                                              
+ *
+ *    *Return: Always 0 (Success)                                                                                                    
+ *
+ *     */                                                                                                                             
+
+                                                                                                                                
+
+int main(void)                                                                                                                  
+
+{                                                                                                                               
+
+	        puts("\"Progamming is like a building a multilingual puzzle") :                                                         
+
+			        return (O) :                                                                                                            
+
+
+					i
